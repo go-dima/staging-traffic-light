d92b03376c648c6be3d19e2dcc0125095def126e
go-dima <dima.gonikman@gmail.com>
2025-01-26 09:24:28 +0200

    install with parameter

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Check if organization parameter is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <organization-name>"
+    exit 1
+fi
+
+ORG_NAME=$1
+
 # Clone the repository
 git clone https://github.com/go-dima/staging-traffic-light.git
 cd staging-traffic-light || exit
@@ -7,10 +15,12 @@ cd staging-traffic-light || exit
 # Install the dependencies
 npm install
 
-# Replace myorg with kodem in public/manifest.json
-sed -i '' 's/myorg/kodem/g' public/manifest.json
+# Replace myorg in public/manifest.json
+sed -i '' "s/myorg/$ORG_NAME/g" public/manifest.json src/utils/constants.ts
 
 # Build
 npm run build
 
-# Chrome -> Extensions -> Load unpacked -> Choose the dist folder
+# Chrome -> Extensions
+#   - Enable developer mode
+#   - Load unpacked -> Choose the dist folder
